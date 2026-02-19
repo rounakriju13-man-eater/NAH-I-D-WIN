cd1e56ce7bcd06ed9cfebcf86e4f2e27e58f2050
rounak <rounak.riju13@gmail.com>
2026-02-19 15:20:28 +0530

    Add files via upload

---
+++ h.w.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String args[]) { 
+        System.out.println("My favorite sport is Cricket");
+        System.out.println("I always play this game when i get free time");
+        System.out.println("Cricket is a outdoor game");
+        System.out.println("It consists of two teams and few players");
+        System.out.println("My favorite cricketer is Virat Kohli");
+        System.out.println("It is one of the most popular sports in the world ");
+    }
+}
