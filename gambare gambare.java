38440c4c65b52e4071323788652a1a4e98ab94ef
rounak <rounak.riju13@gmail.com>
2026-02-19 22:04:59 +0530

    Add files via upload

---
+++ gambare gambare.java
@@ -0,0 +1,14 @@
+ class AddTwoNumbers{
+     public static void main(String[]args){
+            int firstNumber = 67;
+            int secondNumber = 69;
+
+
+            int sum = firstNumber + secondNumber;
+
+             System.out.println("The sum of " + firstNumber + " and " + secondNumber + " is:" + sum);
+
+ 
+     }
+
+ }
